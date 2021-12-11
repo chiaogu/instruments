631be9d99e9faeb457163369d53d56c3e85066f8
chiao <chiaogu@gmail.com>
2021-12-10 17:01:56 -0800

    Set up the project

---
+++ build/serve.js
@@ -0,0 +1,23 @@
+import esbuild from 'esbuild';
+import server from 'live-server';
+import chokidar from 'chokidar';
+import {
+  SRC_PATH,
+  buildOptions,
+  prebuild,
+} from './shared.js'; 
+
+(async () => {
+  await prebuild();
+  const { rebuild } = await esbuild.build({
+    ...buildOptions,
+    incremental: true,
+    sourcemap: true,
+  }); 
+  
+  chokidar
+    .watch(`${SRC_PATH}/**/*.js`, { ignoreInitial: true })
+    .on('all', rebuild);
+    
+  server.start({ root: 'dist' });
+})();
